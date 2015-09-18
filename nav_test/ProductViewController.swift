84f634fc9a75e15de1aa925a60f2fa5b5d38ec1d
mac <mac@macs-MacBook-Air.local>
2015-09-18 04:20:24 -0700

    merging

--- nav_test/ProductViewController.swift
+++ nav_test/ProductViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class ProductViewController: UIViewController {
     var menuItem: Menu!
     
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productDescription: UITextView!
+    @IBOutlet weak var indicaPercentage: UILabel!
+    @IBOutlet weak var sativaPercentage: UILabel!
+    @IBOutlet weak var thcPercentage: UILabel!
+    @IBOutlet weak var growDifficulty: UILabel!
+    @IBOutlet weak var amountSelected: UIPickerView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
